294e52df515a9b4970df93907352e88f44702a76
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-22 17:42:24 -0800

    Pialoud.java

--- src/PiAloud.java
+++ src/PiAloud.java
@@ -5,25 +5,38 @@
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
+import org.omg.Messaging.SyncScopeHelper;
+
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
 public static void main(String[] args) {
 	
-}
+
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-String Pi= 3.14159;
+String Pi= "3.14159";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
-	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+	System.out.println(Pi);
+// 4. Print ALL the digits of of Pi (hint: use a loop)
+for(int i=0;i<Pi.length();i++) {
+	System.out.println(Pi);
+	String x=JOptionPane.showInputDialog("What's the next digit of Pi");
+	char U = x.charAt(0);
+	if(U==2) {
+		System.out.println("correct!");
+	}else {
+		System.out.println("incorrect");
+	}
+	speak(Pi.charAt(i));
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
-
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
